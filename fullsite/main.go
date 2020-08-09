12462b5de6b1aa9916f61a6bc0185209d9cc8083
jason ancrum <jason.ancrum00@delta.com>
2020-08-09 18:52:58 -0400

    new site boilerplate to build

--- fullsite/main.go
+++ fullsite/main.go
@@ -1,56 +1,44 @@
 package main
+
 import (
-	"net/http"
 	"fmt"
-	"time"
-	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-//Create a struct that holds information to be displayed in our HTML file
-type Welcome struct {
-	Name string
-	Time string
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// Declare the static file directory and point it to the directory we just made
+	staticFileDirectory := http.Dir("./assets/")
+	// Declare the handler, that routes requests to their respective filename.
+	// The fileserver is wrapped in the `stripPrefix` method, because we want to
+	// remove the "/assets/" prefix when looking for files.
+	// For example, if we type "/assets/index.html" in our browser, the file server
+	// will look for only "index.html" inside the directory declared above.
+	// If we did not strip the prefix, the file server would look for "./assets/assets/index.html", and yield an error
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	// The "PathPrefix" method acts as a matcher, and matches all routes starting
+	// with "/assets/", instead of the absolute route itself
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
+	return r
 }
 
-//Go application entrypoint
 func main() {
-	//Instantiate a Welcome struct object and pass in some random information.
-	//We shall get the name of the user as a query parameter from the URL
-	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
 
-	//We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
-	// the relative path). We wrap it in a call to template.Must() which handles any errors 
-  // and halts if there are fatal errors
-
-	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
-
-	//Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
-	// a handle that looks in the static directory, go then uses the "/static/" as a url that our
-	//html can refer to when looking for our css and other files.
-
-	http.Handle("/myPath/", //final url can be anything
-		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("static")))) //Go looks in the relative static directory first, then matches it to a
-			//url of our choice as shown in http.Handle("/static/"). 
-      //This url is what we need when referencing our css files
-			//once the server begins. Our html code would therefore be <link rel="stylesheet"  href="/static/stylesheet/...">
-			//It is important to note the final url can be whatever we like, so long as we are consistent.
-
-	//This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
-	http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
-
-		//Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
-		if name := r.FormValue("name"); name != "" {
-			welcome.Name = name;
-		}
-		//If errors show an internal server error message
-		//I also pass the welcome struct to the welcome-template.html file. 
-		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
-	//Print any errors from starting the webserver using fmt
-	fmt.Println(http.ListenAndServe(":8080", nil));
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
