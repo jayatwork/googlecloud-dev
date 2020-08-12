5a9c70dc338a0ebfad165be367a3322eee509bcc
jason ancrum <jason.ancrum00@delta.com>
2020-08-12 01:16:56 -0400

    fat fingered in main program

--- fullsite/main.go
+++ fullsite/main.go
@@ -1,19 +1,21 @@
-gcloud package main
+package main
+
 //Necessar imports for stdlib and external
 import (
-   "net/http"
-   "fmt"
-   "log"
-   "github.com/gorilla/mux"
-   "encoding/json"
-   "net"
-   "strings"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 //Create a struct that holds information to be displayed in our HTML file
 type Welcome struct {
-   Name string
-   Time string
+	Name string
+	Time string
 }
 
 type Article struct {
@@ -24,7 +26,6 @@ type Article struct {
 }
 
 type Articles []Article
-
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!!!! All the business is at landing /web/ ")
@@ -49,27 +50,26 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
-	fmt.Fprintf(w, "Servername: "+key +" STATUS:")
-	
+	fmt.Fprintf(w, "Servername: "+key+" STATUS:")
+
 	//Strip the unnecessary squileys
 	topen := strings.Replace(key, "{", "", -1)
 	tclose := strings.Replace(topen, "}", "", -1)
-	
-	//Do the tcp dialer for realtime stats 
-    conn, err := net.Dial("tcp", tclose + ":3389")
 
-        if err != nil {
-            fmt.Fprintf(w, "SERVER UNREACHABLE \n")
-        return
-        }
+	//Do the tcp dialer for realtime stats
+	conn, err := net.Dial("tcp", tclose+":3389")
 
-    //Print discovery output remote < -- > local OCP orgin
-    fmt.Fprintf(w, "SERVER ALIVE \n\n")
-    fmt.Fprintf(w, "Remote Address : \n" + conn.RemoteAddr().String())
-    //Called from Openshift Paas platform address
-    fmt.Fprintf(w, "OCP caller Address :" + conn.LocalAddr().String())
-	
-	
+	if err != nil {
+		fmt.Fprintf(w, "SERVER UNREACHABLE \n")
+		return
+	}
+
+	//Print discovery output remote < -- > local OCP orgin
+	fmt.Fprintf(w, "SERVER ALIVE \n\n")
+	fmt.Fprintf(w, "Remote Address : \n"+conn.RemoteAddr().String())
+	//Called from Openshift Paas platform address
+	fmt.Fprintf(w, "OCP caller Address :"+conn.LocalAddr().String())
+
 }
 
 func handleRequests() {
@@ -77,7 +77,7 @@ func handleRequests() {
 	staticFileDirectory := http.Dir("./web/")
 	staticFileHandler := http.StripPrefix("/web/", http.FileServer(staticFileDirectory))
 	myRouter.PathPrefix("/web/").Handler(staticFileHandler).Methods("GET")
-	
+
 	//myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/greeting", greetingPage)
 	myRouter.HandleFunc("/all", returnAllArticles)
@@ -86,8 +86,8 @@ func handleRequests() {
 }
 
 func main() {
-   //Report status of webserver and enable handler
-   fmt.Println("Listening");
-   handleRequests()
-   
+	//Report status of webserver and enable handler
+	fmt.Println("Listening")
+	handleRequests()
+
 }
